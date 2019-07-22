0f847430bf34eb079108f8a6ef13730dbc0fe7e0
Amin Bm <medaminbenmbarek@gmail.com>
2019-07-22 11:28:36 +0100

    initial commit: creating project

---
+++ src/main/java/insat/amin/restapireactive/RestApiReactiveApplication.java
@@ -0,0 +1,13 @@
+package insat.amin.restapireactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiReactiveApplication.class, args);
+	}
+
+}
