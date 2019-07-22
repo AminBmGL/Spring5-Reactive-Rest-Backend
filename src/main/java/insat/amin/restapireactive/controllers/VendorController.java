40c935c51066a948cf596e64367fc570b871d807
Amin Bm <medaminbenmbarek@gmail.com>
2019-07-22 15:49:14 +0100

    adding put and patch for category and vendor with related tests

--- src/main/java/insat/amin/restapireactive/controllers/VendorController.java
+++ src/main/java/insat/amin/restapireactive/controllers/VendorController.java
@@ -1,7 +1,14 @@
 package insat.amin.restapireactive.controllers;
 
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import insat.amin.restapireactive.domain.Vendor;
@@ -27,5 +34,31 @@ public Flux<Vendor> getAll() {
 Mono<Vendor> getById(@PathVariable String id){
     return vendorRepository.findById(id);
 }
+
+@ResponseStatus(HttpStatus.CREATED)
+@PostMapping("/api/v1/vendors")
+Mono<Void> create(@RequestBody  Publisher<Vendor> vendorStream) {
+    return vendorRepository.saveAll(vendorStream).then();
+}
+
+@PutMapping("api/v1/vendors/{id}")
+Mono<Vendor> update(@PathVariable String id, @RequestBody Vendor vendor){
+    vendor.setId(id);
+    return vendorRepository.save(vendor);
+}
+
+@PatchMapping("api/v1/vendors/{id}")
+Mono<Vendor> patch(@PathVariable String id, @RequestBody Vendor vendor){
+
+    Vendor foundVendor = vendorRepository.findById(id).block();
+
+    if(!foundVendor.getFirstName().equals(vendor.getFirstName())){
+        foundVendor.setFirstName(vendor.getFirstName());
+
+        return vendorRepository.save(foundVendor);
+    }
+    return Mono.just(foundVendor);
+}
+
 	
 }
--- src/test/java/insat/amin/restapireactive/controllers/CategoryControllerTest.java
+++ src/test/java/insat/amin/restapireactive/controllers/CategoryControllerTest.java
@@ -1,9 +1,16 @@
 package insat.amin.restapireactive.controllers;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import insat.amin.restapireactive.domain.Category;
@@ -48,5 +55,76 @@ public class CategoryControllerTest {
                 .exchange()
                 .expectBody(Category.class);
 
+    }
+    
+    @Test
+    public void testCreateCateogry() {
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().description("descrp").build()));
+
+        Mono<Category> catToSaveMono = Mono.just(Category.builder().description("Some Cat").build());
+
+        webTestClient.post()
+                .uri("/api/v1/categories")
+                .body(catToSaveMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+    
+    @Test
+    public void TestUpdate() {
+        BDDMockito.given(categoryRepository.save(any(Category.class)))
+                .willReturn(Mono.just(Category.builder().build()));
+
+        Mono<Category> toUpdateMono = Mono.just(Category.builder().description("Some desc").build());
+
+        webTestClient.put()
+                .uri("/api/v1/categories/blab")
+                .body(toUpdateMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
+    
+    
+    @Test
+    public void testPatchWithChanges() {
+        given(categoryRepository.findById(anyString()))
+                .willReturn(Mono.just(Category.builder().build()));
+
+        given(categoryRepository.save(any(Category.class)))
+                .willReturn(Mono.just(Category.builder().build()));
+
+        Mono<Category> catToUpdateMono = Mono.just(Category.builder().description("New Description").build());
+
+        webTestClient.patch()
+                .uri("/api/v1/categories/asdfasdf")
+                .body(catToUpdateMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        verify(categoryRepository).save(any());
+    }
+
+    @Test
+    public void testPatchNoChanges() {
+        given(categoryRepository.findById(anyString()))
+                .willReturn(Mono.just(Category.builder().build()));
+
+        given(categoryRepository.save(any(Category.class)))
+                .willReturn(Mono.just(Category.builder().build()));
+
+        Mono<Category> catToUpdateMono = Mono.just(Category.builder().build());
+
+        webTestClient.patch()
+                .uri("/api/v1/categories/asdfasdf")
+                .body(catToUpdateMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        verify(categoryRepository, never()).save(any());
     }
 }
